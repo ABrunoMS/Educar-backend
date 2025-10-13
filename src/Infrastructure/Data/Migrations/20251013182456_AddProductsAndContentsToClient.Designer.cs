49d5f70bd37835d40db967d10515fd08fe326323
Bruno Marques <anderson.bruno90@gmail.com>
2025-10-13 16:19:16 -0300

    Adiciona Produtoos e conteúdos ao Client

--- src/Infrastructure/Data/Migrations/20251013182456_AddProductsAndContentsToClient.Designer.cs
+++ src/Infrastructure/Data/Migrations/20251013182456_AddProductsAndContentsToClient.Designer.cs
@@ -4,17 +4,20 @@ using System.Collections.Generic;
 using Educar.Backend.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Educar.Backend.Infrastructure.Migrations
+namespace Educar.Backend.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251013182456_AddProductsAndContentsToClient")]
+    partial class AddProductsAndContentsToClient
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
